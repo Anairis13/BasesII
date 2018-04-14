eec6192a902ad5e3d8d825d97e7ed0f6a2b4909b
Allan J. Martínez <Jafectnez@gmail.com>
2018-04-13 18:27:08 -0600

    Consultas Clientes y Territorios

--- Consultas para ETL.sql
+++ Consultas para ETL.sql
@@ -5,3 +5,20 @@ INNER JOIN Person.Person B
 ON(A.BusinessEntityID=B.BusinessEntityID)
 INNER JOIN HumanResources.Employee C
 ON (A.BusinessEntityID=C.BusinessEntityID);
+
+--CONSULTA PARA LOS CLIENTES------------------------------------------------------------------------
+SELECT A.CustomerID,
+	   D.FirstName,
+	   D.LastName,
+	   D.Title,
+	   C.Name as Store,
+	   B.Name as Territory,
+	   A.AccountNumber 
+FROM Sales.Customer A
+INNER JOIN Sales.SalesTerritory B
+	ON A.TerritoryID = B.TerritoryID
+INNER JOIN Sales.Store C
+	ON A.StoreID = C.BusinessEntityID
+INNER JOIN Person.Person D
+	ON A.PersonID = D.BusinessEntityID
+ORDER BY A.CustomerID;
